0101101c91d44ca9653c23d6c2d0e8302ce823cd
Alvaro-Fernnadez <alvaro-fernnadez@Alvaro-Fernnadezs-MacBook-Pro.local>
2026-08-12 16:27:06 -0400

    Initial commit

---
+++ functions/api/config.js
@@ -0,0 +1,10 @@
+export async function onRequest(context) {
+
+  return Response.json({
+    tourStartDate: context.env.TOUR_START_DATE || "",
+    showDates: context.env.SHOW_DATES || "false",
+    showCompletedDays: context.env.SHOW_COMPLETED_DAYS || "false",
+    initialLanguage: context.env.INITIAL_LANGUAGE || "es"
+  });
+
+}
